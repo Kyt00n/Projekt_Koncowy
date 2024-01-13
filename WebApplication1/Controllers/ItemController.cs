5714cbba2c5dc11634ef5a9492499d38a07f60f7
Eryk Olsza <eryk.olsza@microsoft.wsei.edu.pl>
2024-01-13 18:44:16 +0100

    Item Controller nad views added

---
+++ WebApplication1/Controllers/ItemController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class ItemController : Controller
+{
+    private readonly WebAplicationDbContext _context;
+
+    public ItemController(WebAplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // //GET: Item
+    // public async Task<IActionResult> Index()
+    // {
+    //     var items = await _context.Items.ToListAsync();
+    //     return View(items);
+    // }
+    public async Task<IActionResult> Index(int? warehouseId)
+    {
+        ViewBag.WarehouseList = new SelectList(_context.Warehouses, "WarehouseID", "WarehouseName");
+
+        var itemsQuery = _context.Items.Include(i => i.Warehouse).AsQueryable();
+
+        if (warehouseId.HasValue)
+        {
+            itemsQuery = itemsQuery.Where(i => i.WarehouseID == warehouseId);
+        }
+
+        var items = itemsQuery.ToList();
+
+        return View(items);
+    }
+
+    // GET: Item/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var item = await _context.Items
+            .FirstOrDefaultAsync(m => m.ItemID == id);
+
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        return View(item);
+    }
+    public IActionResult Create(int? warehouseId)
+    {
+        ViewBag.WarehouseList = new SelectList(_context.Warehouses, "WarehouseID", "WarehouseName");
+
+        return View();
+    }
+    // POST: Item/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("ItemID,ItemName,Description,Price,QuantityInStock,WarehouseID")] Item item)
+    {
+        
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+    }
+}
---
+++ WebApplication1/Controllers/WarehouseController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class WarehouseController : Controller
+{
+    private readonly WebAplicationDbContext _context;
+
+    public WarehouseController(WebAplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Warehouse
+    public async Task<IActionResult> Index()
+    {
+        var warehouses = await _context.Warehouses.ToListAsync();
+        return View(warehouses);
+    }
+
+    // GET: Warehouse/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var warehouse = await _context.Warehouses
+            .Include(w => w.Items)
+            .FirstOrDefaultAsync(m => m.WarehouseID == id);
+
+        if (warehouse == null)
+        {
+            return NotFound();
+        }
+
+        return View(warehouse);
+    }
+
+    // GET: Warehouse/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Warehouse/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Warehouse warehouse)
+    {
+        
+        _context.Add(warehouse);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+        
+    }
+}
